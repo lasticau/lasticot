d4f8fe51f2e170e275e633196aa6ea4b7ac0ee61
auto-reformat <auto-reformat@auto.reformat>
2020-10-08 18:58:36 +0000

    [Auto] Reformat

--- main/Client.cpp
+++ main/Client.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+int main( int argc, char* argv[] )
+{
+    std::cout << "Hello world!" << std::endl;
 
-	std::cout << "Hello world!" << std::endl;
-
-	return 0;
+    return 0;
 }
--- test/test_worm.cpp
+++ test/test_worm.cpp
@@ -1,5 +1,5 @@
-int main(int argc, char* argv[]) {
-
-	// TODO
-	return 0;
+int main( int argc, char* argv[] )
+{
+    // TODO
+    return 0;
 }
